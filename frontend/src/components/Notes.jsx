f0f7f5e8d5bd7c978ce6ab4e7a7cc08795b7d214
Rabi Uddin <rabiuddin1@gmail.com>
2024-08-23 01:18:05 +0500

    Made note keeping app using django and react

---
+++ frontend/src/components/Notes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Notes.css"
+
+function Notes({ note, onDelete }) {
+
+    const formatedDate = new Date(note.created_at).toLocaleDateString("en-US")
+
+  return (
+    <div className="note-container">
+      <p className="note-title">{note.title}</p>
+      <p className="note-content">{note.content}</p>
+      <p className="note-date">{formatedDate}</p>
+      <button className="delete-button" onClick={() => onDelete(note.id)}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default Notes;
